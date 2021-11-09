f187b7e0f0b36b51ddbe9c785bc1bcf03783c4e4
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-09 16:36:08 +0100

    Wikinimous Challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  title = Faker::Movies::StarWars.quote
+  content = Faker::Books::Lovecraft.paragraphs
+  Article.create(title: title, content: content)
+end
